8b11d82cf7c5f902d55dba6f0d6118871cc42d9d
FunctionHub <83389348+FunctionHub-Devlooper@users.noreply.github.com>
2021-12-21 23:19:33 +0300

    index

---
+++ 10e/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1640117850,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/play-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"main.css",
+		"before.css",
+		"start.txt",
+		"play.png"
+	]
+}
